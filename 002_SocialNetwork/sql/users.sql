e739be2eb94b37c80c1bc486dd2a97345ff67d7c
Maciej Wrabel <maciejwrabel@interia.pl>
2021-01-06 00:19:03 +0100

    SQL - more commends

--- 002_SocialNetwork/sql/users.sql
+++ 002_SocialNetwork/sql/users.sql
@@ -1,9 +1,13 @@
 CREATE TABLE users (
 	id SERIAL NOT NULL,
 	username VARCHAR(45),
-	passwowrd VARCHAR(45),
+	password VARCHAR(45),
 	
 	PRIMARY KEY(id)
 );
 
 SELECT *FROM users;
+
+INSERT INTO users (username, password) VALUES
+    ('maciek', 'kot'),
+    ('agata', 'pies');
